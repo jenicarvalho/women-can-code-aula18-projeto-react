5183b47d97c5fddc95165562a3c58c6689b97d92
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-06 17:58:38 -0300

    Updates

---
+++ precache-manifest.f58d49f4bac61044b7f1e13aa4600160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ccd570d5c4677c8bb309c17ec94ba8",
+    "url": "/women-can-code-aula18-projeto-react/index.html"
+  },
+  {
+    "revision": "43d4b64d114304be3747",
+    "url": "/women-can-code-aula18-projeto-react/static/css/main.74e848ea.chunk.css"
+  },
+  {
+    "revision": "463695d8e1ee28bf482c",
+    "url": "/women-can-code-aula18-projeto-react/static/js/2.223480c7.chunk.js"
+  },
+  {
+    "revision": "43d4b64d114304be3747",
+    "url": "/women-can-code-aula18-projeto-react/static/js/main.18a8c837.chunk.js"
+  },
+  {
+    "revision": "752bdb65705aeb987303",
+    "url": "/women-can-code-aula18-projeto-react/static/js/runtime~main.1c5e376c.js"
+  }
+]);
